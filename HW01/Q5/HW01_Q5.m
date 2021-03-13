64888e3b7c62ae9e142c1379c8fe7900c331549e
mohammadraziei <mohammadraziei1375@gmail.com>
2021-03-14 02:43:32 +0330

    upload HW01/Q5

--- HW01/Q5/HW01_Q5.m
+++ HW01/Q5/HW01_Q5.m
@@ -1,9 +1,17 @@
 clc; clear; close all; addpath('../../CommonUtils');
 Questions_folder = '../Questions/';
-image_raw = imread(fullfile(Questions_folder,'Hist.tif'));
-fig = figure('Color','white', 'ToolBar', 'none', 'MenuBar', 'none', 'Name', 'Q2');
+image_raw = imread(fullfile(Questions_folder,'retina.png'));
 
-% save_figure(fig, 'Q2.png')
+sigmoid =@(x,a) 1./(1+exp(a*x));
+L = 255;
 
-
+image = double(image_raw)/L; 
+image = adapthisteq(image);
+image = 1-sigmoid(image,10);
+image = adapthisteq(image);
+image = (image > 0.95);
+image = uint8(image*L);
+fig = create_figure('Q5');
+montage_row({image_raw, image})
+save_figure(fig, 'Q5.png')
 
