dec60c01ee8fed16f3dcb2cb3cbcb4b31bc90b3b
MohammadRaziei <mohammadraziei1375@gmail.com>
2021-05-15 17:41:51 +0430

    remove examin_neightbors

--- HW03/Q2/BilateralFilter.m
+++ HW03/Q2/BilateralFilter.m
@@ -1,41 +1,34 @@
 function image_filt = BilateralFilter(image, hx, hg)
-    [M,N] = size(image);
-    windowSize = ceil(3*hx);
-    image_filt = zeros(M, N);
-    tqdm = ProgressBar(M*N, 'Title', 'Bilateral Filtering');
-    for row = 1:M
-        for col = 1:N
-            X = [row, col];
-            neighbors = Examin_Neighbors(image, X, windowSize);
-            sum_kernel = 0;
-            sum_g_kernel = 0;
-            for i = 1:size(neighbors, 1)
-                Y = neighbors(i,:);
+[M,N] = size(image);
+halfWindowSize = ceil(3*hx);
+image_filt = zeros(M, N);
+tqdm = ProgressBar(M*N, 'Title', 'Bilateral Filtering');
+for row = 1:M
+    for col = 1:N
+        X = [row, col];
+        sum_kernel = 0;
+        sum_g_kernel = 0;
+        % Loop for neighbors:
+        for i = (X(1)-halfWindowSize):(X(1)+halfWindowSize)
+            if i < 1 || i > M || i == X(1), continue; end
+            for j = (X(2)-halfWindowSize):(X(2)+halfWindowSize)
+                if j < 1 || j > N || i == X(2), continue; end
+                Y = [i,j]; % Y: neighbors
                 Gx = image(X(1),X(2));
                 Gy = image(Y(1),Y(2));
                 kernel = Kernel(X, Y, Gx, Gy, hx, hg);
                 sum_kernel = sum_kernel + kernel;
                 sum_g_kernel = sum_g_kernel + kernel * Gy;
             end
-            image_filt(row,col) = sum_g_kernel / sum_kernel;
-            tqdm([], [], []);
         end
+        image_filt(row,col) = sum_g_kernel / sum_kernel;
+        tqdm([], [], []);
     end
-    tqdm.release();
+end
+tqdm.release();
 end
 function out = Kernel(X,Y,Gx,Gy, hx, hg)
-    out = exp(-0.5 * norm(X-Y)^2 / hx) * exp(-0.5 * norm(Gx-Gy)^2 / hg); 
+out = exp(-0.5 * norm(X-Y)^2 / hx) * exp(-0.5 * norm(Gx-Gy)^2 / hg);
 end
-function neighbors = Examin_Neighbors(image, X, halfWindowSize)
-    [M,N] = size(image);
-    x = X(1); y = X(2);
-    neighbors = zeros(0,2);
-    for i = (x-halfWindowSize):(x+halfWindowSize)
-        if i < 1 || i > M || i == x, continue; end
-        for j = (y-halfWindowSize):(y+halfWindowSize)
-            if j < 1 || j > N || i == y, continue; end
-            neighbors = [neighbors;[i,j]];
-        end
-    end
-end
+
 
