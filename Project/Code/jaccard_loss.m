2055fa7ef108f84e19c4c2587b951cbb56b250df
mohammadraziei <mohammadraziei1375@gmail.com>
2021-07-10 03:36:36 +0430

    fix jaccard loss

--- Project/Code/jaccard_loss.m
+++ Project/Code/jaccard_loss.m
@@ -1,5 +1,7 @@
 function ret = jaccard_loss(y_true, y_pred)
 % NOTE: sum(X,'all'): This syntax is valid for MATLAB® versions R2018b and later.
-A = logical(y_true); B = logical(y_pred);
-ret = sum(bsxfun(@times, y_true==y_pred, bsxfun(@times, A, B)), 'all') / (sum(logical(A+B), 'all'));
+% A = logical(y_true); B = logical(y_pred);
+% ret = sum(bsxfun(@times, y_true==y_pred, bsxfun(@times, A, B)), 'all') / (sum(logical(A+B), 'all'));
+DS = dice_loss(y_true, y_pred);
+ret = DS / (2-DS);
 end
