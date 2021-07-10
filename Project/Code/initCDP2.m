ce4066622794e3cbe962f448555cab955a4217eb
mohammadraziei <mohammadraziei1375@gmail.com>
2021-07-10 04:54:34 +0430

    read CPD algorithm and initialize it

---
+++ Project/Code/initCDP2.m
@@ -0,0 +1,10 @@
+function initCDP2()
+CDP2_PATH = '../Modules/CDP2/';
+addpath(fullfile(CDP2_PATH, 'data'))
+addpath(fullfile(CDP2_PATH, 'core'))
+addpath(fullfile(CDP2_PATH, 'core/utils'))
+addpath(fullfile(CDP2_PATH, 'core/Rigid'))
+addpath(fullfile(CDP2_PATH, 'core/Nonrigid'))
+addpath(fullfile(CDP2_PATH, 'core/FGT'))
+addpath(fullfile(CDP2_PATH, 'core/mex'))
+end
